b9a7cfe40a770174baf3879d40877291d7b81859
SadiyehRezaii-dev <rezaiisadiyeh@gmail.com>
2025-03-30 19:34:35 +0200

    Add welcome message and menu to todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,11 @@
+print("📝 Welcome to your simple To-Do List!")
 
+# Creat an empty list to store tasks
+todo_list = []
+
+# Show menu options
+print("\nChoose an option:")
+print("1. Add a task")
+print("2. view tasks")
+print("3. Mark task as done")
+print("4. Exit")
