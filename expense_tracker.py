6229a14de1ed61890ed058b1acbf3d32b7797ced
SadiyehRezaii-dev <rezaiisadiyeh@gmail.com>
2025-03-30 21:33:04 +0200

    start of expense_tracker.py with welcome and menu

---
+++ expense_tracker.py
@@ -0,0 +1,13 @@
+# welcome the user 
+print(" 💰 Welcome to your Simple Expense Tracker!")
+
+# Creat an empty list to store expenses
+
+expenses = []
+
+# Show menu
+print("\nChoose an option:")
+print("1. Add an expense")
+print("2. View all expenses")
+print("3. View total spent")
+print("4. Exit")
